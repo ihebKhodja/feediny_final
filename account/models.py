138eadb9ff53f44d2af4f50ccc7a7f7526854f1a
iheb <iheb.khodja@gmail.com>
2021-06-12 13:11:58 +0100

    upload

--- account/models.py
+++ account/models.py
@@ -27,65 +27,8 @@ class Client(models.Model):
         return self.user.username
 
 
-# class UserProfileManager(BaseUserManager):
-#
-#     def create_user(self, email, name, password=None):
-#
-#         if not email:
-#             raise ValueError('Users must have an email address.')
-#
-#         email = self.normalize_email(email)
-#         user = self.model(email=email, name=name,)
-#
-#         user.set_password(password)
-#         user.save(using=self._db)
-#
-#         return user
-#
-#     def create_superuser(self, email, name, password):
-#
-#         user = self.create_user(email, name, password)
-#
-#         user.is_superuser = True
-#         user.is_staff = True
-#         user.save(using=self._db)
-#
-#         return user
 
 
-# class Manager(AbstractBaseUser, PermissionsMixin):
-#
-#     email = models.EmailField(max_length=255, unique=True)
-#     name = models.CharField(max_length=255)
-#     is_active = models.BooleanField(default=True)
-#     is_staff = models.BooleanField(default=False)
-#
-#     objects = UserProfileManager()
-#
-#     USERNAME_FIELD = 'email'
-#     REQUIRED_FIELDS = ['name']
-#
-#
-#     def __str__(self):
-#
-#         return self.email
 
 
-# class Client(AbstractBaseUser, PermissionsMixin):
-#
-#     email = models.EmailField(max_length=255, unique=True)
-#     username = models.CharField(max_length=255, unique=True)
-#     name = models.CharField(max_length=255)
-#     phone_number = models.CharField(max_length=10)
-#     is_active = models.BooleanField(default=True)
-#     is_staff = models.BooleanField(default=False)
-#
-#     objects = UserProfileManager()
-#
-#     USERNAME_FIELD = 'username'
-#     REQUIRED_FIELDS = ['name']
-#
-#
-#     def __str__(self):
-#
-#         return self.phone_number
+
