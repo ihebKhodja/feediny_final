3628ab6e9eddc4a286073d7877391ecb126c1884
iheb <iheb.khodja@gmail.com>
2021-06-13 12:39:15 +0100

    upload

--- deliver/urls.py
+++ deliver/urls.py
@@ -4,14 +4,13 @@ from . import views
 app_name = 'deliver'
 
 urlpatterns = [
-    path('restaurant/', views.RestaurantList.as_view(), name='c_restaurant'),  # create restaurant
+    path('restaurant/', views.RestaurantList.as_view(), name='c_restaurant'),  # list create restaurant
     path('restaurant/<int:pk>', views.RestaurantDetail.as_view(), name='r_u_d_restaurant'),# retrieve update delete restaurant
     path('meal/', views.MealCreate.as_view(), name='create_meal'),
     path('meal/list/<int:pk>', views.MealList.as_view(), name='listByIdMeal'),
     path('meal/<int:restaurant>', views.MealSearch.as_view(), name='ByRestaurantId_meal'),
     path('cart/', views.CartList.as_view(), name='CartList'),### get list
     path('cart/add/', views.CartAdd.as_view(), name='AddtoCart'),#### post create new cart
-    # path('cart/add/<int:pk>', views.AddMealToCart.as_view(), name='Ad_Meal_Cart'),  #### post add meal to cart
     path('cart/<int:pk>', views.CartModifier.as_view(), name='Update_Delete_Retrieve_Cart'),#### post delete get methods
     path('order/add/', views.OrderAdd.as_view(), name='AddtoOrder'),  #### post
     path('order/<int:restaurant>', views.OrderSearch.as_view(), name='ByRestaurantId_order'),#### orders by restaurant ID
